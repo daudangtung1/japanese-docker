6486408425a8ca66e36a0562bc2a16a3769dd28c
hieu <hieuhoa1408@gmail.com>
2021-03-31 09:44:48 +0700

    exam create

--- project/resources/views/admin/entity/exam/create.blade.php
+++ project/resources/views/admin/entity/exam/create.blade.php
@@ -1,3 +1,44 @@
+<p class="text-center text-xl">Ban dang lam phan nghe cua de thi n2</p>
 <form>
-<h1>tét</h1>
+    <div class="flex space-x-3 mb-10 mt-10 text-sm font-medium">
+        <p>Phan Lua Chon :</p>
+        <div class="flex-auto flex space-x-3">
+            <button wire:click="vocabulary()" class="w-1/4 flex items-center justify-center rounded-md border border-gray-300" type="button">Tu Vung</button>
+            <button wire:click="listening()" class="w-1/4 flex items-center justify-center rounded-md border border-gray-300" type="button">Nghe</button>
+            <button wire:click="read()" class="w-1/4 flex items-center justify-center rounded-md border border-gray-300" type="button">Doc Hieu</button>
+            <button wire:click="full()" class="w-1/4 flex items-center justify-center rounded-md bg-black text-white" type="button">Tron de</button>
+        </div>
+    </div>
+    <div class="flex space-x-3 mb-10 mt-10 text-sm font-medium">
+        <p>Level :</p>
+        <div class="flex-auto flex space-x-3">
+            <button class="w-1/5 flex items-center justify-center rounded-md border border-gray-300" type="submit">N1</button>
+            <button class="w-1/5 flex items-center justify-center rounded-md border border-gray-300" type="button">N2</button>
+            <button class="w-1/5 flex items-center justify-center rounded-md bg-black text-white" type="submit">N3</button>
+            <button class="w-1/5 flex items-center justify-center rounded-md border border-gray-300" type="submit">N4</button>
+            <button class="w-1/5 flex items-center justify-center rounded-md border border-gray-300" type="submit">N5</button>
+        </div>
+    </div>
+    <div class="flex space-x-3 mb-10 mt-10 text-sm font-medium">
+        <p>All Question : 180 cau</p>
+    </div>
+
+    @switch($full_mode)
+        @case($listen_mode)
+            @include('admin.entity.exam.mode.list-listen')
+        @break
+
+            @case($vocabulary_mode)
+            @include('admin.entity.exam.mode.list-vocabulary')
+        @break
+
+        @case($read_mode)
+            @include('admin.entity.exam.mode.list-read')
+        @break
+
+        @default
+        @include('admin.entity.exam.mode.list-full-mode')
+    @endswitch
+
+    <button wire:click.prevent="store()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
 </form>
---
+++ project/resources/views/admin/entity/exam/mode/list-vocabulary.blade.php
@@ -0,0 +1,121 @@
+<p class="text-center text-xl">Vocabulary </p>
+<div class="border-2 border-red-500 mb-10 mt-10">
+    <p class="text-center text-xl">Cau so 1</p>
+    <div class="form-group">
+        <label class="block">
+            <span class="text-gray-900">Question :</span>
+            <textarea class="form-textarea mt-1 block w-full" rows="3" placeholder="Enter some long form content."></textarea>
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="1">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="2">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="3">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="4">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <label class="block">
+                <span class="text-gray-700">Dap An Dung</span>
+                <select class="form-multiselect block w-full mt-1" multiple>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </label>
+    </div>
+</div>
+<div class="border-2 border-red-500 mb-10 mt-10">
+    <p class="text-center text-xl">Cau so 1</p>
+    <div class="form-group">
+        <label class="block">
+            <span class="text-gray-900">Question :</span>
+            <textarea class="form-textarea mt-1 block w-full" rows="3" placeholder="Enter some long form content."></textarea>
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="1">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="2">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="3">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="4">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <label class="block">
+                <span class="text-gray-700">Dap An Dung</span>
+                <select class="form-multiselect block w-full mt-1" multiple>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </label>
+    </div>
+</div>
+<div class="border-2 border-red-500 mb-10 mt-10">
+    <p class="text-center text-xl">Cau so 1</p>
+    <div class="form-group">
+        <label class="block">
+            <span class="text-gray-900">Question :</span>
+            <textarea class="form-textarea mt-1 block w-full" rows="3" placeholder="Enter some long form content."></textarea>
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="1">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="2">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="3">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <span class="text-gray-900">Answer :</span>
+            <input class="shadow appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="4">
+        </label></br>
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+        <label class="block">
+            <label class="block">
+                <span class="text-gray-700">Dap An Dung</span>
+                <select class="form-multiselect block w-full mt-1" multiple>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </label>
+    </div>
+</div>
